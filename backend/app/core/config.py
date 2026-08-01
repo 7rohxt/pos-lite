19cd329d3c6a63daeadc0e099f2c12c4c0a9f5e8
Rohit Karthick <rkbeo5@gmail.com>
2026-08-01 13:46:23 +0530

    feat: add JWT configuration settings

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -7,23 +7,11 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
     DATABASE_URL: str
-    # JWT settings will be added here later:
-    # SECRET_KEY: str
-    # ALGORITHM: str = "HS256"
-    # ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+    # JWT 
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
 
 settings = Settings() # This is where the objects are created 
-
-
-# Why separate config file?
-# Without a dedicated config file:
-
-# database.py
-# DATABASE_URL = os.getenv(...)
-# auth.py
-# SECRET_KEY = os.getenv(...)
-# email.py
-# SMTP_USER = os.getenv(...)
-
-# Now configuration is spread everywhere.
