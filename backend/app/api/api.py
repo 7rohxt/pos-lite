3d5cad3af1d87f9cac0afee8d5b1dad563e561e2
Rohit Karthick <rkbeo5@gmail.com>
2026-08-03 12:32:45 +0530

    feat: add admin router to API

--- backend/app/api/api.py
+++ backend/app/api/api.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter
 
-from app.api.routes import products, auth, sales
+from app.api.routes import products, auth, sales, admin
 
 api_router = APIRouter()
 
 api_router.include_router(products.router)
 api_router.include_router(auth.router)
 api_router.include_router(sales.router)
-# api_router.include_router(admin.router)   # later
+api_router.include_router(admin.router)
